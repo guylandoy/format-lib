f8fbd0dc73da0f0c5579b692af00676162ee13ff
guylandoy <guylandoy@gmail.com>
2020-09-24 16:07:29 +0300

    Added some comments.

--- formatlib/formatter.py
+++ formatlib/formatter.py
@@ -6,6 +6,7 @@ class Formatter:
     def __init__(self, data):
         self.data = json.loads(data)
 
+    # Convert columns to timestamp UTC tz.
     def convert_dates(self, column_names, input_formats):
         if type(column_names) != list or type(input_formats) != list \
                 or len(column_names) != len(input_formats) or len(column_names) <= 0:
@@ -13,9 +14,9 @@ class Formatter:
         # Update every column in json to the result of the date_formatter.
         for x in range(len(column_names)):
             self.data = self.convert_date(column_names[x], input_formats[x])
-            # json_data[column_names[x]] = date_formatter(column_names[x], input_formats[x])
         return self.data
 
+    # Convert a column to timestamp UTC tz.
     def convert_date(self, column_name, input_format):
         formatted_result = date_to_timestamp(self.data[column_name], input_format)
         self.data[column_name] = formatted_result
--- formatlib/utils.py
+++ formatlib/utils.py
@@ -6,20 +6,24 @@ wgs84 = CRS("EPSG:4326")  # LatLon with WGS84 datum used by GPS unit
 UTM36N = CRS("EPSG:32636")
 
 
+# Convert tuple of (latitude, longitude) to object
 def get_lat_lon_object_from_tuple(lat_lon_tuple):
     return {'latitude': lat_lon_tuple[0], 'longitude': lat_lon_tuple[1]}
 
 
+# convert Date to timestamp of UTC timezone according to format given (format of datetime.strptime)
 def date_to_timestamp(date, input_format):
     return datetime.strptime(date, input_format).replace(tzinfo=timezone.utc).timestamp()
 
 
+# Convert utm easting and northing coordinates to Latitude and Longitude using utm package.
 def utm_to_lat_long(easting, northing):
     lat_lon = utm.to_latlon(easting, northing, 36, 'N')
     return get_lat_lon_object_from_tuple(lat_lon)
-    # return {'latitude': lat_lon[0], 'longitude': lat_lon[1]}
 
 
+# Use pyproj.Proj to get latitude and longitude
+# based on given projection (proj) and ellipsoid (ellps) with 2 coordinates
 def coordinates_proj(proj, ellps, coordinate1, coordinate2):
     if proj == 'utm':
         p = Proj(proj='utm', zone=36, ellps=ellps, preserve_units=False)
@@ -27,6 +31,7 @@ def coordinates_proj(proj, ellps, coordinate1, coordinate2):
         return get_lat_lon_object_from_tuple((lat, lng))
 
 
+# Convert utm easting and northing using pyproj.transform from UTM Zone 36N, to WGS84 latitude and longitude.
 def utm_to_wgs84(easting, northing):
     return get_lat_lon_object_from_tuple(transform(UTM36N, wgs84, easting, northing))
 
