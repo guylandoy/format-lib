a83501015d0a2a538fb55e1db81de42b77fb0074
guylandoy <guylandoy@gmail.com>
2020-09-30 13:55:44 +0300

    Added flatten and standardize tests

--- tests/tests_myfunctions.py
+++ tests/tests_myfunctions.py
@@ -1,9 +1,8 @@
-
-
 def format_key(key):
     return key+'_formatted'
 
 
+# Date Conversion test
 def test1():
     from formatlib import formatter
     json_sample = '{ "dob":"31/12/1997", "age":30, "city":"New York"}'
@@ -12,6 +11,7 @@ def test1():
     assert res[format_key("dob")] == 883526400.0
 
 
+# Date Conversion test - multiple keys
 def test2():
     from formatlib import formatter
     json_sample = '{ "dob":"31/12/1997", "age":30, "city":"New York", "date":"Fri, 02 Feb 1996 03:04:05 GMT"}'
@@ -20,6 +20,7 @@ def test2():
     assert res[format_key("dob")] == 883526400 and res[format_key("date")] == 823230245
 
 
+# UTM Conversion test
 def test3():
     from formatlib import formatter
     with open('data_samples/test3.json', 'r') as json_file:
@@ -31,6 +32,7 @@ def test3():
         and res[format_key("UTM_2")]["latitude"] == 34.771670 and res[format_key("UTM_2")]["longitude"] == 32.092158
 
 
+# UTM Conversion test
 def test4():
     from formatlib import formatter
     with open('data_samples/test4.json', 'r') as json_file:
@@ -42,6 +44,7 @@ def test4():
         and res[format_key("UTM_2")]["latitude"] == 32.015281 and res[format_key("UTM_2")]["longitude"] == 34.748833
 
 
+# UTM Conversion test - different names and types.
 def test5():
     from formatlib import formatter
     with open('data_samples/test5.json', 'r') as json_file:
@@ -53,6 +56,7 @@ def test5():
         and res[format_key("UTM_2")]["latitude"] == 32.015281 and res[format_key("UTM_2")]["longitude"] == 34.748833
 
 
+# UTM Conversion test
 def test6():
     from benedict import benedict
     from formatlib import formatter
@@ -67,6 +71,7 @@ def test6():
            and res[format_key("a.b.c.loc")]["longitude"] == 34.748833
 
 
+# UTM Conversion test - Northing has 6 digits only (the first digit missing)
 def test7():
     from benedict import benedict
     from formatlib import formatter
@@ -81,6 +86,7 @@ def test7():
            and res[format_key("a.b.c.loc")]["longitude"] == 34.748833
 
 
+# WGS Conversion test
 def test8():
     from benedict import benedict
     from formatlib import formatter
@@ -88,16 +94,34 @@ def test8():
         formatter = formatter.Formatter(json_file)
 
     res = benedict(formatter.convert_wgs("geometry.coordinates"))
-    print("TEST8:", res)
     assert res[format_key("geometry.coordinates")]["latitude"] == 31.781882 \
            and res[format_key("geometry.coordinates")]["longitude"] == 34.804687
 
 
+# Flatten test
 def test9():
-    from benedict import benedict
     from formatlib import formatter
     with open('data_samples/test9.json', 'r') as json_file:
         formatter = formatter.Formatter(json_file)
-    res = benedict(formatter.flatten_json())
+    res = formatter.flatten_json()
     assert res["a_b_c"] == 1
+
+
+# Standardize test
+def test10():
+    from formatlib import formatter
+    with open('data_samples/test10.json', 'r') as json_file:
+        formatter = formatter.Formatter(json_file)
+    res = formatter.standardize()
+    assert res['position_ahead']['no_standard']['names_and_looks'] == 1
+
+
+# Standardize and Flatten test
+def test11():
+    from formatlib import formatter
+    with open('data_samples/test10.json', 'r') as json_file:
+        formatter = formatter.Formatter(json_file)
+    formatter.standardize()
+    res = formatter.flatten_json()
+    assert res['position_ahead_no_standard_names_and_looks'] == 1
 
