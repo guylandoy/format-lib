2b5ed157cb823a542b608df3ac4d502b902c3eac
guylandoy <guylandoy@gmail.com>
2020-09-30 13:40:54 +0300

    Updated package setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import find_packages, setup
 setup(
     name='formatlib',
-    packages=find_packages(include=['formatter']),
+    packages=find_packages(),
     version='0.1.0',
-    description='Format JSON data',
+    description='Format JSON data - location and date  -> DE Standards',
     author='GuyDE',
     license='MIT',
     test_suite='tests',
+    url='https://github.com/guylandoy/format-lib',
+    install_requires=['pyproj', 'python-benedict'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+
 )
 
